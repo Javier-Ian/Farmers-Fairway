5b9ee9bbf8cdf72a4054fa3d74dc9447f742bca3
Baldivino467 <2201102976@student.buksu.edu.ph>
2024-11-10 13:07:27 +0800

    Naay Admin Role

--- register.php
+++ register.php
@@ -1,6 +1,8 @@
 <?php
 
-include 'config.php';
+@include 'config.php';
+
+session_start();
 
 if(isset($_POST['submit'])){
 
@@ -12,35 +14,25 @@ if(isset($_POST['submit'])){
    $pass = filter_var($pass, FILTER_SANITIZE_STRING);
    $cpass = md5($_POST['cpass']);
    $cpass = filter_var($cpass, FILTER_SANITIZE_STRING);
-
-   $image = $_FILES['image']['name'];
-   $image = filter_var($image, FILTER_SANITIZE_STRING);
-   $image_size = $_FILES['image']['size'];
-   $image_tmp_name = $_FILES['image']['tmp_name'];
-   $image_folder = 'uploaded_img/'.$image;
+   $user_type = $_POST['user_type'];
+   $user_type = filter_var($user_type, FILTER_SANITIZE_STRING);
 
    $select = $conn->prepare("SELECT * FROM `users` WHERE email = ?");
    $select->execute([$email]);
 
    if($select->rowCount() > 0){
-      $message[] = 'user email already exist!';
+      $message[] = 'user email already exists!';
    }else{
       if($pass != $cpass){
          $message[] = 'confirm password not matched!';
       }else{
-         $insert = $conn->prepare("INSERT INTO `users`(name, email, password, image) VALUES(?,?,?,?)");
-         $insert->execute([$name, $email, $pass, $image]);
+         $insert = $conn->prepare("INSERT INTO `users`(name, email, password, user_type) VALUES(?,?,?,?)");
+         $insert->execute([$name, $email, $pass, $user_type]);
 
          if($insert){
-            if($image_size > 2000000){
-               $message[] = 'image size is too large!';
-            }else{
-               move_uploaded_file($image_tmp_name, $image_folder);
-               $message[] = 'registered successfully!';
-               header('location:login.php');
-            }
+            $message[] = 'registered successfully!';
+            header('location:login.php');
          }
-
       }
    }
 
@@ -82,19 +74,22 @@ if(isset($message)){
    
 <section class="form-container">
 
-   <form action="" enctype="multipart/form-data" method="POST">
+   <form action="" method="POST">
       <h3>register now</h3>
       <input type="text" name="name" class="box" placeholder="enter your name" required>
       <input type="email" name="email" class="box" placeholder="enter your email" required>
       <input type="password" name="pass" class="box" placeholder="enter your password" required>
       <input type="password" name="cpass" class="box" placeholder="confirm your password" required>
-      <input type="file" name="image" class="box" required accept="image/jpg, image/jpeg, image/png">
+      <select name="user_type" class="box" required>
+         <option value="">Select your role</option>
+         <option value="user">Customer</option>
+         <option value="admin">Buyer</option>
+      </select>
       <input type="submit" value="register now" class="btn" name="submit">
       <p>already have an account? <a href="login.php">login now</a></p>
    </form>
 
 </section>
-
 
 </body>
 </html>
